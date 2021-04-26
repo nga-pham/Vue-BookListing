9b08aab852a1939154b9f2a353dae30e30714e82
ngapham <nga.pham.cs83@gmail.com>
2021-04-26 14:41:03 +0200

    add a bookItem

---
+++ src/components/BookItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <li v-for="book in books" :key="book.title">
+        {{ book.title }}:{{ book.author }}
+      </li>
+</template>
+<script>
+export default {
+    name: "BookItem",
+    props: ['books']
+}
+</script>
+<style scoped>
+li {
+  display: block;
+  margin: 0 10px;
+}
+</style>
